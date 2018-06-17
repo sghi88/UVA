2d4e4374e0b2340f4a2bc09b9aa393e9fbba4617
sghi88 <40232706+sghi88@users.noreply.github.com>
2018-06-17 23:28:35 +0200

    Update uvatest.py

--- uvatest.py
+++ uvatest.py
@@ -96,7 +96,6 @@ plt.savefig("plot1.pdf")
 
 #####PLOT 2###################
 fig = plt.figure(figsize=(7, 20), dpi=100)
-
 ax1 = fig.add_axes([0.1, 0.85, 0.8, 0.11])
 ax2 = fig.add_axes([0.1, 0.74, 0.8, 0.11])
 ax3 = fig.add_axes([0.1, 0.63, 0.8, 0.11])
@@ -105,33 +104,44 @@ ax5 = fig.add_axes([0.1, 0.41, 0.8, 0.11])
 ax6 = fig.add_axes([0.1, 0.30, 0.8, 0.11])
 ax7 = fig.add_axes([0.1, 0.19, 0.8, 0.11])
 ax8 = fig.add_axes([0.1, 0.08, 0.8, 0.11])
-
 ax1.plot(dates, data_red[:,0])
+ax1.set_ylabel('size bin 0')
 ax2.plot(dates, data_red[:,1])
+ax2.set_ylabel('size bin 1')
 ax3.plot(dates, data_red[:,2])
+ax3.set_ylabel('size bin 2')
 ax4.plot(dates, data_red[:,3])
+ax4.set_ylabel('size bin 3')
 ax5.plot(dates, data_red[:,4])
+ax5.set_ylabel('size bin 4')
 ax6.plot(dates, data_red[:,5])
+ax6.set_ylabel('size bin 5')
 ax7.plot(dates, data_red[:,6])
+ax7.set_ylabel('size bin 6')
 ax8.plot(dates, data_red[:,7])
-
 ax8.set_xlabel('time')
+ax8.set_ylabel('size bin 7')
 plt.xticks(rotation = 25)
 plt.savefig("plot2.pdf")
 #####PLOT 2###################
-
 #####PLOT 3###################
 fig2 = plt.figure(figsize=(7, 20), dpi=100)
-
 ax9 = fig2.add_axes([0.1, 0.85, 0.8, 0.11])
+ax9.set_ylabel('size bin 8')
 ax10 = fig2.add_axes([0.1, 0.74, 0.8, 0.11])
+ax10.set_ylabel('size bin 9')
 ax11 = fig2.add_axes([0.1, 0.63, 0.8, 0.11])
+ax11.set_ylabel('size bin 10')
 ax12 = fig2.add_axes([0.1, 0.52, 0.8, 0.11])
+ax12.set_ylabel('size bin 11')
 ax13 = fig2.add_axes([0.1, 0.41, 0.8, 0.11])
+ax13.set_ylabel('size bin 12')
 ax14 = fig2.add_axes([0.1, 0.30, 0.8, 0.11])
+ax14.set_ylabel('size bin 13')
 ax15 = fig2.add_axes([0.1, 0.19, 0.8, 0.11])
+ax15.set_ylabel('size bin 14')
 ax16 = fig2.add_axes([0.1, 0.08, 0.8, 0.11])
-
+ax16.set_ylabel('size bin 15')
 ax9.plot(dates, data_red[:,8])
 ax10.plot(dates, data_red[:,9])
 ax11.plot(dates, data_red[:,10])
@@ -140,21 +150,17 @@ ax13.plot(dates, data_red[:,12])
 ax14.plot(dates, data_red[:,13])
 ax15.plot(dates, data_red[:,14])
 ax16.plot(dates, data_red[:,15])
-
 ax16.set_xlabel('time')
 plt.xticks(rotation = 25)
 plt.savefig("plot3.pdf")
 #####PLOT 4###################
 fig3 = plt.figure(figsize=(7, 12), dpi=100)
-
 ax1 = fig3.add_axes([0.1, 0.66, 0.8, 0.28])
 ax2 = fig3.add_axes([0.1, 0.38, 0.8, 0.28])
 ax3 = fig3.add_axes([0.1, 0.1, 0.8, 0.28])
-
 ax1.plot(dates, data_red[:,-3])
 ax2.plot(dates, data_red[:,-2])
 ax3.plot(dates, data_red[:,-1])
-
 ax3.set_xlabel('time')
 ax1.set_ylabel('PM1 [ug/m3])')
 ax2.set_ylabel('PM2.5 [ug/m3]')
@@ -162,21 +168,20 @@ ax3.set_ylabel('PM10 [ug/m3]')
 plt.xticks(rotation = 25)
 plt.savefig("plot4.pdf")
 #####PLOT 4###################
-
 #####PLOT 5###################
 fig4 = plt.figure()
-
 plt.semilogy(dates, data_red[:,0])
 plt.semilogy(dates, data_red[:,3])
 plt.semilogy(dates, data_red[:,6])
 plt.semilogy(dates, data_red[:,9])
 plt.semilogy(dates, data_red[:,12])
 plt.semilogy(dates, data_red[:,15])
-
+plt.xlabel('time')
+plt.ylabel('sizebin')
 plt.legend(["bin1", "bin4", "bin7", "bin10", "bin13", "bin16"])
 plt.xticks(rotation = 25)
 plt.savefig("plot5.pdf")
 #####PLOT 5###################
-
 #plt.show()
-"""
+
+
